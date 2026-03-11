a229ad990bb0b88f43cbc9e4770d7f20444649a4
stacybradford-ctrl <stacy.bradford@gmail.com>
2026-03-11 17:53:00 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import PickleballDirector from "../components/PickleballDirector";
+
+export default function Page() {
+  return (
+    <main style={{ padding: 20 }}>
+      <PickleballDirector />
+    </main>
+  );
+}
